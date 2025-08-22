eda853a6bf61d9be68ca0394cde1faa66c2e1259
Xiang Kai <xk231op@gmail.com>
2025-08-22 14:29:24 +0800

    Rename bot to Beta and add skeleton code

---
+++ src/main/java/Beta.java
@@ -0,0 +1,8 @@
+public class Beta {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Beta\n" + "What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
