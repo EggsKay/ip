26600ef6fc5ea3e8032e160c2ddca6a235bc7d7f
Xiang Kai <xk231op@gmail.com>
2025-08-28 23:20:53 +0800

    Add echo function with bye termination

--- src/main/java/Beta.java
+++ src/main/java/Beta.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Beta {
     public static void main(String[] args) {
-
+        String line = "";
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Beta\n" + "What can I do for you?\n");
-
-        System.out.println("Bye. Hope to see you again soon!");
+        while (!line.equals("bye")) {
+            line = in.nextLine();
+            System.out.println(line +"\n");
+        }
+        System.out.println("Bye. See you soon!");
     }
 }
