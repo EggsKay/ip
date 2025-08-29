cf999ebc59a5e6ef0eda071ce13cf0ee564b2321
Xiang Kai <xk231op@gmail.com>
2025-08-29 13:33:41 +0800

    Add list function

--- src/main/java/Beta.java
+++ src/main/java/Beta.java
@@ -1,13 +1,31 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Beta {
     public static void main(String[] args) {
         String line = "";
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Beta\n" + "What can I do for you?\n");
-        while (!line.equals("bye")) {
-            line = in.nextLine();
-            System.out.println(line +"\n");
+        String[] list = new String[100];
+        int listSize = 0;
+        while (!(line = in.nextLine()).equals("bye")) {
+            switch (line) {
+
+            case "list":
+                for (int i = 1; i <= listSize; i++) {
+                    System.out.println(i + ". " + list[i - 1]);
+                }
+                System.out.println("");
+                break;
+            case "bye":
+
+                break;
+            default:
+                System.out.println("Added: " + line + "\n");
+                list[listSize] = line;
+                listSize++;
+                break;
+            }
         }
         System.out.println("Bye. See you soon!");
     }
