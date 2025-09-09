336719f918cc9761267ac4ceaf2770b6ce540270
Xiang Kai <xk231op@gmail.com>
2025-09-09 20:30:46 +0800

    refactored and improved code quality

--- src/main/java/Beta.java
+++ src/main/java/Beta.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Beta {
+
+    private static final int MAX_TASKS = 100;
+    private static final Task[] tasks = new Task[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello! I'm Beta\nWhat can I do for you?\n");
-        Task[] list = new Task[100];
-        int listSize = 0;
+        greetUser();
 
         while (true) {
             String line = in.nextLine();
@@ -17,66 +20,92 @@ public class Beta {
             String inputBody = (parts.length > 1) ? parts[1] : "";
 
             switch (command) {
-
             case "todo":
-                list[listSize] = new Todo(inputBody);
-                System.out.println("Nice added this: \n" + list[listSize].toString() + "\n");
-                listSize++;
+                handleTodo(inputBody);
                 break;
-
             case "deadline":
-                int byIndex = inputBody.indexOf(" /by ");
-                String deadlineTask = inputBody.substring(0, byIndex);
-                String deadline = inputBody.substring(byIndex + 5);
-                list[listSize] = new Deadline(deadlineTask, deadline);
-                System.out.println("Nice added this: \n" + list[listSize].toString() + "\n");
-                listSize++;
+                handleDeadline(inputBody);
                 break;
-
             case "event":
-                int fromIndex = inputBody.indexOf(" /from ");
-                int toIndex = inputBody.indexOf(" /to ");
-                String eventTask = inputBody.substring(0, fromIndex);
-                String from = inputBody.substring(fromIndex + 7, toIndex);
-                String to = inputBody.substring(toIndex + 5);
-                list[listSize] = new Event(eventTask, from, to);
-                System.out.println("Nice added this: \n" + list[listSize].toString() + "\n");
-                listSize++;
+                handleEvent(inputBody);
                 break;
-
             case "list":
-                if (listSize == 0) {
-                    System.out.println("YAY!! Good Job!! There is nothing to do.");
-                }
-                for (int i = 1; i <= listSize; i++) {
-                    System.out.println(i + ". " + list[i - 1].toString());
-                }
-                System.out.println("");
+                handleList();
                 break;
-            case "mark": {
-                int index = Integer.parseInt(inputBody) - 1;
-                if (index >= 0 && index < listSize) {
-                    list[index].markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(list[index] + "\n");
-                } else {
-                    System.out.println("Invalid task number.\n");
-                }
-            }
-            break;
-            case "unmark": {
-                int index = Integer.parseInt(inputBody) - 1;
-                if (index >= 0 && index < listSize) {
-                    list[index].unmarkAsDone();
-                    System.out.println("Ok I've unmarked this task:");
-                    System.out.println(list[index].toString() + "\n");
-                } else {
-                    System.out.println("Invalid task number.\n");
-                }
+            case "mark":
+                handleMark(inputBody);
                 break;
-            }
+            case "unmark":
+                handleUnmark(inputBody);
+                break;
             }
         }
+        exitProgram();
+    }
+
+    private static void exitProgram() {
         System.out.println("Bye. See you soon!");
+    }
+
+    private static void handleUnmark(String inputBody) {
+        int index = Integer.parseInt(inputBody) - 1;
+        if (index >= 0 && index < taskCount) {
+            tasks[index].unmarkAsDone();
+            System.out.println("Ok I've unmarked this task:");
+            System.out.println(tasks[index].toString() + "\n");
+        } else {
+            System.out.println("Invalid task number.\n");
+        }
+    }
+
+    private static void handleMark(String inputBody) {
+        int index = Integer.parseInt(inputBody) - 1;
+        if (index >= 0 && index < taskCount) {
+            tasks[index].markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(tasks[index] + "\n");
+        } else {
+            System.out.println("Invalid task number.\n");
+        }
+    }
+
+    private static void handleList() {
+        if (taskCount == 0) {
+            System.out.println("YAY!! Good Job!! There is nothing to do.");
+        }
+        for (int i = 1; i <= taskCount; i++) {
+            System.out.println(i + ". " + tasks[i - 1].toString());
+        }
+        System.out.println("");
+    }
+
+    private static void handleEvent(String inputBody) {
+        int fromIndex = inputBody.indexOf(" /from ");
+        int toIndex = inputBody.indexOf(" /to ");
+        String eventTask = inputBody.substring(0, fromIndex);
+        String from = inputBody.substring(fromIndex + 7, toIndex);
+        String to = inputBody.substring(toIndex + 5);
+        tasks[taskCount] = new Event(eventTask, from, to);
+        System.out.println("Nice added this: \n" + tasks[taskCount].toString() + "\n");
+        taskCount++;
+    }
+
+    private static void handleDeadline(String inputBody) {
+        int byIndex = inputBody.indexOf(" /by ");
+        String deadlineTask = inputBody.substring(0, byIndex);
+        String deadline = inputBody.substring(byIndex + 5);
+        tasks[taskCount] = new Deadline(deadlineTask, deadline);
+        System.out.println("Nice added this: \n" + tasks[taskCount].toString() + "\n");
+        taskCount++;
+    }
+
+    private static void handleTodo(String inputBody) {
+        tasks[taskCount] = new Todo(inputBody);
+        System.out.println("Nice added this: \n" + tasks[taskCount].toString() + "\n");
+        taskCount++;
+    }
+
+    private static void greetUser() {
+        System.out.println("Hello! I'm Beta\nWhat can I do for you?\n");
     }
 }
